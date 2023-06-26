2a0fcab33ded6069e014fa8d8f40fda0b571dbbc
GrahamDo <graham@communicate.za.net>
2023-06-26 12:00:41 +0200

    Allow setting values from command line

--- TootAnalyticsEmailer/TootAnalyticsEmailer/Program.cs
+++ TootAnalyticsEmailer/TootAnalyticsEmailer/Program.cs
@@ -5,6 +5,12 @@
         static void Main(string[] args)
         {
             var settings = Settings.Load();
+            if (args.Length == 3 && args[0].ToLower() == "--set")
+            {
+                settings.SetValueFromArguments(args[1], args[2]);
+                settings.Save();
+                return;
+            }
         }
     }
 }
