6e781a269933e1dfa68fb477dd0f2f1a575ea190
GrahamDo <graham@communicate.za.net>
2023-07-01 12:32:59 +0200

    Add SMTP Specific settings

--- TootAnalyticsEmailer/TootAnalyticsEmailer/Settings.cs
+++ TootAnalyticsEmailer/TootAnalyticsEmailer/Settings.cs
@@ -1,4 +1,5 @@
-﻿using Newtonsoft.Json;
+﻿using System.Net.Mail;
+using Newtonsoft.Json;
 
 namespace TootAnalyticsEmailer
 {
@@ -9,12 +10,28 @@ namespace TootAnalyticsEmailer
         public string InstanceUrl { get; set; }
         public string Token { get; set; }
         public string AccountName { get; set; }
+        public string SmtpServer { get; set; }
+        public ushort SmtpPort { get; set; }
+        public bool SmtpEnableSsl { get; set; }
+        public string SmtpUsername { get; set; }
+        public string SmtpPassword { get; set; }
+        public string EmailSubject { get; set; }
+        public string EmailFromAddress { get; set; }
+        public string EmailFromDisplayName { get; set; }
+        public Dictionary<string, string> ToEmailAddresses { get; set; }
 
         public Settings()
         {
             InstanceUrl = string.Empty;
             Token = string.Empty;
             AccountName = string.Empty;
+            SmtpServer = string.Empty;
+            SmtpUsername = string.Empty;
+            SmtpPassword = string.Empty;
+            EmailSubject = string.Empty;
+            EmailFromAddress = string.Empty;
+            EmailFromDisplayName = string.Empty;
+            ToEmailAddresses = new Dictionary<string, string>();
         }
 
         public static Settings Load()
@@ -35,19 +52,53 @@ namespace TootAnalyticsEmailer
 
         public void SetValueFromArguments(string setting, string value)
         {
-            switch (setting.ToLower())
+            try
             {
-                case "instanceurl":
-                    InstanceUrl = value;
-                    break;
-                case "token":
-                    Token = value;
-                    break;
-                case "accountname":
-                    AccountName = value;
-                    break;
-                default:
-                    throw new ApplicationException($"Invalid setting: {setting}");
+                switch (setting.ToLower())
+                {
+                    case "instanceurl":
+                        InstanceUrl = value;
+                        break;
+                    case "token":
+                        Token = value;
+                        break;
+                    case "accountname":
+                        AccountName = value;
+                        break;
+                    case "smtpserver":
+                        SmtpServer = value;
+                        break;
+                    case "smtpport":
+                        SmtpPort = Convert.ToUInt16(value);
+                        break;
+                    case "smtpenablessl":
+                        SmtpEnableSsl = Convert.ToBoolean(value);
+                        break;
+                    case "smtpusername":
+                        SmtpUsername = value;
+                        break;
+                    case "smtppassword":
+                        SmtpPassword = value;
+                        break;
+                    case "emailsubject":
+                        EmailSubject = value;
+                        break;
+                    case "toemailaddresses":
+                        var list = value.Split(";");
+                        foreach (var item in list)
+                        {
+                            var parts = item.Split(',');
+                            ToEmailAddresses.Add(parts[0], parts[1]);
+                        }
+
+                        break;
+                    default:
+                        throw new ApplicationException($"Invalid setting: {setting}");
+                }
+            }
+            catch (FormatException)
+            {
+                throw new ApplicationException("Invalid value for setting");
             }
         }
     }
