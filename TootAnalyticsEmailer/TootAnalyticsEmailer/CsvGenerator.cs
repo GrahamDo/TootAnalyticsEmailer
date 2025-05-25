4d8fc39be4aa8001d3a7c64d04e75095193b3e49
GrahamDo <downs_graham@yahoo.com>
2025-05-25 12:28:38 +0200

    Indicate boosted statuses in CSV

--- TootAnalyticsEmailer/TootAnalyticsEmailer/CsvGenerator.cs
+++ TootAnalyticsEmailer/TootAnalyticsEmailer/CsvGenerator.cs
@@ -8,13 +8,14 @@ internal class CsvGenerator
     public string GenerateFromStatuses(List<MastodonStatus> statuses)
     {
         var result = new StringBuilder();
-        result.AppendLine("\"Created At\",\"URL\",\"Content\",\"Hashtags\",\"Attachments\",\"Boosts\",\"Favourites\",\"Replies\"");
+        result.AppendLine("\"Created At\",\"Is Boost?\",\"URL\",\"Content\",\"Hashtags\",\"Attachments\",\"Boosts\",\"Favourites\",\"Replies\"");
         statuses.ForEach(s =>
         {
-            var url = s.Reblog != null ? s.Reblog.Url : s.Url;
-            var content = s.Reblog != null ? s.Reblog.Content : s.Content;
+            var isBoost = s.Reblog != null;
+            var url = isBoost ? s.Reblog.Url : s.Url;
+            var content = isBoost ? s.Reblog.Content : s.Content;
             result.AppendLine(
-                $"\"{s.CreatedAt}\",\"{url}\",\"{content}\",\"{s.HashTags}\",\"{s.HasAttachments}\",\"{s.Boosts}\",\"{s.Favourites}\",\"{s.Replies}\"");
+                $"\"{s.CreatedAt}\",\"{isBoost}\",\"{url}\",\"{content}\",\"{s.HashTags}\",\"{s.HasAttachments}\",\"{s.Boosts}\",\"{s.Favourites}\",\"{s.Replies}\"");
         });
 
         return result.ToString();
