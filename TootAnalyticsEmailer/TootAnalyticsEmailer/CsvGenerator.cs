9dccf5f7efbd3753e1ab43a331bd68630430c2ff
GrahamDo <downs_graham@yahoo.com>
2025-05-25 13:15:08 +0200

    For boosts, Show counts from original toot

--- TootAnalyticsEmailer/TootAnalyticsEmailer/CsvGenerator.cs
+++ TootAnalyticsEmailer/TootAnalyticsEmailer/CsvGenerator.cs
@@ -14,8 +14,11 @@ internal class CsvGenerator
             var isBoost = s.Reblog != null;
             var url = isBoost ? s.Reblog.Url : s.Url;
             var content = isBoost ? s.Reblog.Content : s.Content;
+            var boosts = isBoost ? s.Reblog.Boosts : s.Boosts;
+            var favourites = isBoost ? s.Reblog.Favourites : s.Favourites;
+            var replies = isBoost ? s.Reblog.Replies : s.Replies;
             result.AppendLine(
-                $"\"{s.CreatedAt:yyyy-MM-dd HH:mm:ss}\",\"{isBoost}\",\"{url}\",\"{content}\",\"{s.HashTags}\",\"{s.HasAttachments}\",\"{s.Boosts}\",\"{s.Favourites}\",\"{s.Replies}\"");
+                $"\"{s.CreatedAt:yyyy-MM-dd HH:mm:ss}\",\"{isBoost}\",\"{url}\",\"{content}\",\"{s.HashTags}\",\"{s.HasAttachments}\",\"{boosts}\",\"{favourites}\",\"{replies}\"");
         });
 
         return result.ToString();
