3593523bee9c53963349741bce9f5eca7b43ee80
GrahamDo <downs_graham@yahoo.com>
2026-05-30 11:31:39 +0200

    Abstract classes do not work with Newtonsoft

--- TootAnalyticsEmailer/TootAnalyticsEmailer/Models/MastodonHashTag.cs
+++ TootAnalyticsEmailer/TootAnalyticsEmailer/Models/MastodonHashTag.cs
@@ -1,8 +1,9 @@
 ﻿// ReSharper disable AutoPropertyCanBeMadeGetOnly.Global
-// All properties must have public getters and setters for serialisation to work
+// ReSharper disable ClassNeverInstantiated.Global
+// All properties must have public getters and setters, and all classes must be concrete, for serialisation to work
 namespace TootAnalyticsEmailer.Models;
 
-internal abstract class MastodonHashTag
+internal class MastodonHashTag
 {
     public string Name { get; set; } = string.Empty;
 }
